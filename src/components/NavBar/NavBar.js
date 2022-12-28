8916167e54f6489c32a540cc35b780a44c260e85
Rabbi Ahmed Limon <rabbiahemed>
2022-12-28 23:22:04 +0600

    google signin

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -33,17 +33,6 @@ const NavBar = () => {
             <Link to="completed-task" className="text-white m-1">
               Completed Task
             </Link>
-            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
           </Nav>
           <Nav>
             {user ? (
