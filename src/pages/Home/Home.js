3f4a40fc7cd35f5a2feb95e7623c284e29de0870
Rabbi Ahmed Limon <rabbiahemed>
2022-12-29 16:18:59 +0600

    task table

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,12 @@
 import { React, useEffect } from "react";
-import { toast } from "react-hot-toast";
+import { toast, Toaster } from "react-hot-toast";
 
 const Home = () => {
   const handleKeyDown = (event) => {
     if (event.key === "Enter") {
       const task = event.target.value;
       const data = { taskname: task };
+      const notify = (message) => toast(message);
       // fetch
 
       fetch("http://localhost:5000/tasks", {
@@ -17,7 +18,9 @@ const Home = () => {
       })
         .then((response) => response.json())
         .then((data) => {
+          notify("Task added successfully");
           console.log("Success:", data);
+          event.target.value = "";
         })
         .catch((error) => {
           console.error("Error:", error);
@@ -40,10 +43,11 @@ const Home = () => {
       <input
         className="mx-auto w-100 my-2 rounded-pill p-2"
         type="text"
-        placeholder="                                                   write and press enter key to add your next task"
+        placeholder="                                               write and press enter key to add your next task"
         onKeyDown={handleKeyDown}
         required
       />
+      <Toaster />;
     </div>
   );
 };
--- src/pages/MyTask/MyTask.js
+++ src/pages/MyTask/MyTask.js
@@ -1,19 +1,54 @@
-import React from "react";
-import { Button, Card } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 import Table from "react-bootstrap/Table";
+
 const MyTask = () => {
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/tasks")
+      .then((response) => response.json())
+      .then((data) => setTasks(data));
+  }, []);
+
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Details</Button>
-      </Card.Body>
-    </Card>
+    <Table className="my-5 text-center">
+      <tbody>
+        {tasks?.length &&
+          tasks?.map((task, index) => (
+            <tr key={task?._id}>
+              <td>
+                <b>{task?.taskname}</b>
+              </td>
+              <td>
+                <Button variant="success" size="sm">
+                  completed
+                </Button>
+              </td>
+              <td>
+                <Button variant="warning" size="sm">
+                  Update
+                </Button>
+              </td>
+              <td>
+                <Button variant="danger" size="sm">
+                  delete
+                </Button>
+              </td>
+
+              {/* <td>
+                  {booking && !booking.paid && (
+                    <Link to={`/dashboard/payment/${booking._id}`}>
+                      <Button>Pay</Button>
+                    </Link>
+                  )}
+                  {booking && booking.paid && (
+                    <span className="fw-bold">Paid</span>
+                  )}
+                </td> */}
+            </tr>
+          ))}
+      </tbody>
+    </Table>
   );
 };
 
